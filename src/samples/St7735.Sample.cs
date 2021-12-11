68365bc9f40e060f0c041c6d2cad4906caec360c
devbotas <simon@devbot.lt>
2021-12-11 20:08:29 +0200

    Sample now uses SixLabors 2D library to draw text'n'stuff.

--- src/samples/St7735.Sample.cs
+++ src/samples/St7735.Sample.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Device.Spi;
 using System.Threading;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Fonts;
 using Tevux.Device.St7735;
+using SixLabors.ImageSharp.Drawing.Processing;
+using System.Runtime.InteropServices;
 
 // Preparing SPI bus.
 var spiSettings = new SpiConnectionSettings(0, 1);
@@ -21,53 +27,48 @@ Console.WriteLine("Let's go!");
 // Clearing the screen.
 lcd.Clear();
 
-// Preparing some buffers that will be heavily reused later.
-var randomNumber = new Random();
-var whiteRectangleBuffer = new byte[2 * 10 * 5];
-for (var i = 0; i < 2 * 10 * 5; i++) {
-    whiteRectangleBuffer[i] = 0xFF;
-}
-
-var greenRectangleBuffer = new byte[2 * 5 * 10];
-for (var i = 0; i < 2 * 5 * 10; i += 2) {
+// If you want, you can draw all the pizels yourself, without using 3rd party libraries. Like so:
+var greenRectangleBuffer = new byte[2 * 10 * 10]; // <-- Color is coded BGR565, so two bytes per pixel.
+for (var i = 0; i < 2 * 10 * 10; i += 2) {
     greenRectangleBuffer[i] = 0b00011111;
 }
+lcd.SetRegion(lcd.ActualWidth / 2 - 5, lcd.ActualHeight / 2 - 5, 10, 10);
+lcd.SendBitmap(greenRectangleBuffer);
 
-for (var i = 0; i < 4; i++) {
-    lcd.SetOrientation((global::Tevux.Device.St7735.Orientation)i);
-
-    lcd.Clear();
-
-    // Painting some markers;
-    lcd.SetRegion(0, 0, 10, 5);
-    lcd.SendBitmap(whiteRectangleBuffer);
-    lcd.SetRegion(lcd.ActualWidth - 10, 0, 10, 5);
-    lcd.SendBitmap(whiteRectangleBuffer);
-
-    lcd.SetRegion(0, lcd.ActualHeight - 10, 5, 10);
-    lcd.SendBitmap(greenRectangleBuffer);
-    lcd.SetRegion(lcd.ActualWidth - 5, lcd.ActualHeight - 10, 5, 10);
-    lcd.SendBitmap(greenRectangleBuffer);
-
-    Thread.Sleep(1000);
+// If we want to be fancy,. we can use SixLabors libraries with all the bell and whistles. Like, to draw text with antialiasing.
+// Need to pick a font. These are different for different OSes.
+var font = "DejaVu Sans"; // <-- This works on Rapsberry Pi.
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+    font = "Consolas";
 }
+var fontsys = SystemFonts.CreateFont(font, 16, FontStyle.Regular);
 
-
-// Unleash the demo! Painting random rectangles all over the screen.
+// Some variables to be reused.
+var helloImage = new Image<Bgr565>(80, 16);
+var randomNumber = new Random();
 while (true) {
     Thread.Sleep(100);
+    // At random Y coordinate, and in random color...
+    var randomY = randomNumber.Next(lcd.ActualHeight / 16) * 16;
+    var randomColor = Color.FromRgb((byte)randomNumber.Next(0xFF), (byte)randomNumber.Next(0xFF), (byte)randomNumber.Next(0xFF));
+    var randomBackgroundColor = Color.FromRgb((byte)randomNumber.Next(0xFF), (byte)randomNumber.Next(0xFF), (byte)randomNumber.Next(0xFF));
 
-    var randomX = randomNumber.Next(lcd.ActualWidth - 10);
-    var randomY = randomNumber.Next(lcd.ActualHeight - 10);
+    // ...we'll write hello like there's no tomorrow!
+    helloImage.Mutate(ctx => ctx.Fill(randomBackgroundColor).DrawText("Hello!", fontsys, randomColor, new SixLabors.ImageSharp.PointF(0, 0)));
 
-    lcd.SetRegion(randomX, randomY, 10, 5);
-    lcd.SendBitmap(whiteRectangleBuffer);
+    lcd.SetRegion(0, randomY, 80, 20);
+    lcd.SendBitmap(helloImage.ToBgr565Array());
+}
 
-    Thread.Sleep(100);
-
-    randomX = randomNumber.Next(lcd.ActualWidth - 10);
-    randomY = randomNumber.Next(lcd.ActualHeight - 10);
-
-    lcd.SetRegion(randomX, randomY, 5, 10);
-    lcd.SendBitmap(greenRectangleBuffer);
+public static class SixLaborsExtensions {
+    public static byte[] ToBgr565Array(this Image<Bgr565> image) {
+        var returnArray = new byte[image.Width * image.Height * 2];
+        for (var j = 0; j < image.Height; j++) {
+            for (var i = 0; i < image.Width; i++) {
+                returnArray[2 * (i + image.Width * j)] = (byte)(image[i, j].PackedValue >> 8);
+                returnArray[2 * (i + image.Width * j) + 1] = (byte)image[i, j].PackedValue;
+            }
+        }
+        return returnArray;
+    }
 }
